d86d12e2e4f00694b121967c6764e65bfe96a79e
Jonathan Brooks <jonbrooks0122@gmail.com>
2023-08-31 22:31:33 -0700

    started code challenge

---
+++ javascript/trees/k-ary-search.js
@@ -0,0 +1,42 @@
+class Node {
+  constructor(value) {
+    this.value = value;
+    this.children = [];
+  }
+}
+
+function traversal() {
+  let current = this.root;
+  let funQueue = [];
+  if (current) funQueue.unshift(current);
+  while (funQueue.length) {
+    let dequeuedNode = funQueue.pop();
+    dequeuedNode.children.forEach((child) => funQueue.unshift(child));
+  }
+}
+
+function fizzBuzzTree(root) {
+  if (!root) {
+    return null;
+  }
+
+  const newRoot = new Node(convertValue(root.value));
+
+  for (const child of root.children) {
+    newRoot.children.push(fizzBuzzTree(child));
+  }
+
+  return newRoot;
+}
+
+function convertValue(value) {
+  if (value % 3 === 0 && value % 5 === 0) {
+    return "FizzBuzz";
+  } else if (value % 3 === 0) {
+    return "Fizz";
+  } else if (value % 5 === 0) {
+    return "Buzz";
+  } else {
+    return value.toString();
+  }
+}
